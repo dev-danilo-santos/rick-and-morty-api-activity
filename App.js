c5fb28f4490598c0eabdd919e1008d1671bf581e
Danilo Santos <danilo99ambrosio@hotmail.com>
2023-04-17 12:06:58 -0300

    Deletando componente inutilizado

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import { StatusBar } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import CharsEffectScreen from './src/screens/chars-effect'
-import MenuScreen from './src/screens/menu'
-import MainCharsScreen from './src/screens/main-chars'
 import CharacterDetailScreen from './src/screens/character-detail'
 import LocationDetailScreen from './src/screens/location-details'
 import EpisodeScreen from './src/screens/episodes'
@@ -14,7 +12,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="CharsEffect">
-        <Stack.Screen name="MainChars" component={MainCharsScreen} />
         <Stack.Screen name="CharsEffect" component={CharsEffectScreen} />
         <Stack.Screen name="CharacterDetail" component={CharacterDetailScreen} />
         <Stack.Screen name="LocationDetail" component={LocationDetailScreen} />
